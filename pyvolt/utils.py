89caae753713a5cb7049fb8d5fad445beb8585da
Gael <69738055+Gael-devv@users.noreply.github.com>
2022-01-21 15:06:13 -0500

    utils

---
+++ pyvolt/utils.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from typing import Any
+
+
+class _MissingSentinel:
+    def __eq__(self, other):
+        return False
+
+    def __bool__(self):
+        return False
+
+    def __repr__(self):
+        return '...'
+
+
+MISSING: Any = _MissingSentinel()
