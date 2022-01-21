8e2f7ed633cd366e2780bb6ef2258f88152cfdc8
Gael <69738055+Gael-devv@users.noreply.github.com>
2022-01-21 16:20:48 -0500

    token usage in environment variables

--- tests/static_login.py
+++ tests/static_login.py
@@ -1,11 +1,19 @@
+from dotenv import load_dotenv
+from os import getenv
+
 import pyvolt
 import asyncio
+
+
+load_dotenv()
+
+REVOLT_TOKEN = getenv("BOT_TOKEN")
 
 
 async def main():
     http_client = pyvolt.http.HTTPClient(loop=asyncio.get_event_loop())
     
-    token = pyvolt.Token("AMcm_dFZNCE-roKk_N6rcPs-mbtGAhb-nLzC1m3kJVCqKcakSEFRC04TcvHN5qGE")
+    token = pyvolt.Token(REVOLT_TOKEN)
     print(await http_client.static_login(token))
     
     print(http_client.api_info)
