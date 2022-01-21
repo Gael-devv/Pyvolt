6389e0b2869715ff2f4ccee4b7f123f25002de63
Gael <69738055+Gael-devv@users.noreply.github.com>
2022-01-21 15:10:35 -0500

    init package

---
+++ pyvolt/__init__.py
@@ -0,0 +1,34 @@
+"""
+Revolt API wrapper
+~~~~~~~~~~~~~~~~~~~
+
+A basic wrapper for the Revolt API.
+
+:copyright: (c) 2022 Gael
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = 'pyvolt'
+__author__ = 'github.com/Gael-devv'
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2022 Gael'
+__version__ = '0.0.1a'
+
+from typing import NamedTuple, Literal
+
+from .token import *
+from .http import *
+from .errors import *
+from .file import *
+from . import utils
+
+
+class VersionInfo(NamedTuple):
+    major: int
+    minor: int
+    micro: int
+    release_level: Literal["alpha", "beta", "candidate", "final"]
+    serial: int
+
+
+version_info: VersionInfo = VersionInfo(major=0, minor=0, micro=1, release_level='alpha', serial=0)
