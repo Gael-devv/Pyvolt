cdef57bc41e7b79c3501a95f10848fb0a4a1d529
Gael <69738055+Gael-devv@users.noreply.github.com>
2022-01-22 13:44:34 -0500

    spoiler=True

--- tests/message_management.py
+++ tests/message_management.py
@@ -11,7 +11,7 @@ async def main():
     token = pyvolt.Token(getenv("BOT_TOKEN"))
     await http_client.static_login(token)
     
-    file = pyvolt.File("C:\\Users\\gaelp\\Downloads\\R.jpg")
+    file = pyvolt.File("C:\\Users\\gaelp\\Downloads\\R.jpg", spoiler=True)
     message = await http_client.send_message("01FSMFSXJBYAAYSVCW9JGXXCJ0", "testing 123", attachment=file)
     await http_client.edit_message(message["channel"], message["_id"], "testing 456")
     
