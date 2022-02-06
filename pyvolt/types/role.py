9a81dc26e939c0da87ee3d3d63852752ec368e46
Gael <69738055+Gael-devv@users.noreply.github.com>
2022-02-05 20:58:53 -0500

    added id as optional field

--- pyvolt/types/role.py
+++ pyvolt/types/role.py
@@ -1,17 +1,21 @@
 from __future__ import annotations
 
-from typing import TypedDict, Tuple
+from typing import TYPE_CHECKING, TypedDict, Tuple
+
+if TYPE_CHECKING:
+    from .snowflake import Snowflake
 
 __all__ = (
-    "Permission",
+    "Permissions",
     "Role",
 )
 
 
-Permission = Tuple[int, int]
+Permissions = Tuple[int, int]
 
 
 class _RoleOptional(TypedDict, total=False):
+    id: Snowflake
     colour: str
     hoist: bool
     rank: int
@@ -19,4 +23,4 @@ class _RoleOptional(TypedDict, total=False):
 
 class Role(_RoleOptional):
     name: str
-    permissions: Permission
+    permissions: Permissions
