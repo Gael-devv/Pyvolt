7a2f6545e5a6c833c2adf12fdc4e7de482af8f3d
Gael <69738055+Gael-devv@users.noreply.github.com>
2022-02-14 21:11:52 -0500

    added util function: colour

--- pyvolt/utils.py
+++ pyvolt/utils.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any
+from typing import Any, Union
 
 
 class _MissingSentinel:
@@ -15,3 +15,26 @@ class _MissingSentinel:
 
 
 MISSING: Any = _MissingSentinel()
+
+
+def colour(value: Union[str, tuple]) -> str:
+    """
+    to convert the string or tuple into valid html colors. 
+    Usage::
+    
+        colour((187, 64, 255))
+        
+        colour((187, 64, 255, 1))
+        
+        colour("BB40FF")
+    """
+    if isinstance(value, tuple):
+        if len(value) == 4:
+            return "rgba" + str(value)
+        else:
+            return "rgb" + str(value)
+    elif isinstance(value, str):
+        if value.startswith("#"):
+            return value
+        else:
+            return "#" + value
